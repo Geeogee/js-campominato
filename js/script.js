e6ffb8d7d87509af6b87fae629a4b604cffd5c70
Gioele <gioele.armiento@gmail.com>
2021-03-15 15:01:35 +0100

    Added a versione using only one cycle, and not 2 annidate

--- js/script.js
+++ js/script.js
@@ -8,36 +8,63 @@ function generateRndNumb(min,max) {
 
 
 function repsCheck(numberToCheck, gameOverNumbers) {
-    var lng = gameOverNumbers.length;
-    var repFound = false;
-    do {
-        for (var i=0; i<lng; i++) {
 
-            var elem = gameOverNumbers[i];
-            if(numberToCheck == elem) {
+    // Annidate cycles version
+    // Check if the random number generated
+    // is already inside the array
+    // In that case, it generates a new number that will be checked
+    // When the number isn't a repetitions
+    // The function returns the number that will be pushed inside the array
+
+    // do {
+    //     for (var i=0; i<lng; i++) {
+
+    //         var elem = gameOverNumbers[i];
+    //         if(numberToCheck == elem) {
                 
-                repFound = true;
-                numberToCheck = generateRndNumb(1,100);
-                break;
-            } else {
+    //             repFound = true;
+    //             numberToCheck = generateRndNumb(1,100);
+    //             break;
+    //         } else {
 
-                repFound = false;  
-            }
+    //             repFound = false; 
+    //         }
+    //     }
+    // } while(repFound)
+
+    // One Cycle version
+    // Check if the number is a repetitions, and return true in that case
+    var repFound = false;
+    for (var i=0; i<gameOverNumbers.length; i++) {
+
+        var elem = gameOverNumbers[i];
+        if(elem == numberToCheck) {
+
+            repFound = true;
+            break;
         }
-    } while(repFound)
-
-    return numberToCheck;
+    }
+    return repFound;
 }
 
+
+// The game (you lost)
 function mineField() {
     var gameOverNumbers = [ ];
-    for (var i=0; i<16; i++) {
-        var rndNumber = generateRndNumb(1,100);
-        var numberChecked = repsCheck(rndNumber, gameOverNumbers);
-        gameOverNumbers.push(numberChecked);
+    
+    while (gameOverNumbers.length < 16) {
+
+        var rndNum = generateRndNumb(1,100);
+        var repsChecked = repsCheck(rndNum, gameOverNumbers);
+        if(!repsChecked) {
+
+            gameOverNumbers.push(rndNum);
+        }
     }
 
-    console.log(gameOverNumbers);
+    console.log(gameOverNumbers.length, gameOverNumbers);
+
+    
 }
 
-mineField();
+// mineField();
